b3559bf9020f4b5c1fe58e5d12f69a93bd4ae3c8
IsaMontoya17 <isabelamontoya2021@gmail.com>
2026-08-19 14:16:29 -0500

    Move configuration files into src/config

--- src/config/config.js
+++ src/config/config.js
@@ -6,4 +6,9 @@ module.exports = {
   CLAVE_SISTEMA: process.env.CLAVE_SISTEMA,
   TIMEOUT_ESPERA: parseInt(process.env.TIMEOUT_ESPERA || '180', 10),
   CARPETA_PRINCIPAL: process.env.CARPETA_PRINCIPAL || 'envio_correos_mensuales',
+
+  EMAIL_SERVICIO: process.env.EMAIL_SERVICIO || 'gmail',
+  EMAIL_USUARIO: process.env.EMAIL_USUARIO,
+  EMAIL_CLAVE: process.env.EMAIL_CLAVE,
+  EMAIL_REMITENTE_NOMBRE: process.env.EMAIL_REMITENTE_NOMBRE || 'Gerencia de Catastro - Gobernación de Antioquia',
 };
