73d8a905f8e80eaf2522d20724e4948bdbf0ca2d
IsaMontoya17 <isabelamontoya2021@gmail.com>
2026-08-25 14:52:57 -0500

    Wrap app with Ant Design ConfigProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ConfigProvider } from 'antd';
+import esES from 'antd/locale/es_ES';
+import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ConfigProvider locale={esES} theme={{ token: { colorPrimary: '#1e293b' } }}>
+      <App />
+    </ConfigProvider>
   </StrictMode>,
-)
+);
